7f2d9717567cf61e501029b1eadd131faeda67ae
Arnarkari93 <arnarkari93@gmail.com>
2025-06-01 12:40:01 +0000

    feat(git): git keybinds

--- .config/nvim/lua/arnar/plugins/fugitive.lua
+++ .config/nvim/lua/arnar/plugins/fugitive.lua
@@ -1,6 +1,6 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.keymap.set('n', '<c-g>', ':Git', { noremap = true, silent = false })
+    vim.keymap.set('n', '<c-g>', ':Git <cr>', { noremap = true, silent = false })
   end,
 }
--- .config/nvim/lua/arnar/plugins/gitsigns.lua
+++ .config/nvim/lua/arnar/plugins/gitsigns.lua
@@ -3,6 +3,11 @@ return {
   config = function()
     require('gitsigns').setup()
 
-    vim.keymap.set('n', "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set('n', "<leader>hp", ":Gitsigns preview_hunk<CR>", {desc = "Preview hunk"})
+    vim.keymap.set('n', "<leader>hr", ":Gitsigns reset_hunk<CR>", {desc = "Reset hunk"})
+    vim.keymap.set('n', "<leader>hn", ":Gitsigns next_hunk<CR>zz", {desc = "Next hunk"})
+    vim.keymap.set('n', "<leader>hN", ":Gitsigns prev_hunk<CR>zz", {desc = "Prev hunk"})
+    vim.keymap.set('n', "<leader>hs", ":Gitsigns stage_hunk<CR>zz", {desc = "Stage hunk"})
+    vim.keymap.set('n', "<leader>hS", ":Gitsigns undo_stage_hunk<CR>zz", {desc = "Undo stage hunk"})
   end
 }
