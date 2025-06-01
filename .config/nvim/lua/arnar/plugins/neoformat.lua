03357e6ba3f94a95a143c8a47e430a97ba8a4e67
Arnarkari93 <arnarkari93@gmail.com>
2025-06-01 12:37:50 +0000

    feat(neoformat): format lua

--- .config/nvim/lua/arnar/plugins/neoformat.lua
+++ .config/nvim/lua/arnar/plugins/neoformat.lua
@@ -15,8 +15,19 @@ return {
     local format_js_ts_group = vim.api.nvim_create_augroup('FormatJSTypes', { clear = true })
     vim.api.nvim_create_autocmd('BufWritePre', {
       group = format_js_ts_group,
-      pattern = { '*.js', '*.ts', '*.tsx' },
+      pattern = { '*.js', '*.ts', '*.tsx', '*.json', '*.mjs' },
       command = 'Neoformat prettier'
     })
+
+    -- vim.api.nvim_create_autocmd('BufWritePre', {
+    --   pattern = { '*.lua' },
+    --   callback = function()
+    --     -- Format buffer if there are changes and avoid an infinite loop
+    --     local clients = vim.lsp.get_active_clients({ bufnr = vim.api.nvim_get_current_buf() })
+    --     if #clients > 0 then
+    --       vim.lsp.buf.format({ async = true })
+    --     end
+    --   end,
+    -- })
   end,
 }
